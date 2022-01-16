75fd9b814fd57dabc9e507ab41c6c83dc86eca69
Josh Struve <63620595+joshstruve@users.noreply.github.com>
2022-01-15 20:42:14 -0800

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function onWindowResize() {
 
 var camera = new THREE.PerspectiveCamera( 20, window.innerWidth / window.innerHeight, 1, 500 );
 
-camera.position.set(0, 2, 39);
+camera.position.set(0, 2, 29);
 
 var scene = new THREE.Scene();
 var city = new THREE.Object3D();
